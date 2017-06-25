8b041a9ba94adade5dea161b64481c8fddbe48fa
harry <harry@vinova.sg>
2017-06-25 11:58:15 +0700

    refactor base dialog

--- code/base/src/main/java/vn/eazy/base/mvp/base/fragment/BaseFragment.java
+++ code/base/src/main/java/vn/eazy/base/mvp/base/fragment/BaseFragment.java
@@ -1,9 +1,7 @@
 package vn.eazy.base.mvp.base.fragment;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
